4a62ec2c1775204aea1fc9d3615f40dec69f1288
VivianeLopes <v.almeida.do.carmo@avanade.com>
2024-04-26 17:18:44 -0300

    commit mcv

---
+++ ProjetoVivianeCurso/CursoEAlunos/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEAlunos.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoVivianeCurso/CursoEAlunos/Controllers/CursoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEAlunos.Controllers
+{
+    public class CursoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
